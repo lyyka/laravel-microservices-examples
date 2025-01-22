f1b1e7f2825312f62ba2bfca0965a0afc2c3f4d6
Luka Robajac <luka.robajac@gmail.com>
2025-01-22 21:47:03 +0100

    use http pool in api gateway

--- api-gateway/gateway/app/Http/Controllers/GatewayController.php
+++ api-gateway/gateway/app/Http/Controllers/GatewayController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Client\Pool;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -10,11 +11,14 @@ class GatewayController extends Controller
 {
     public function __invoke(Request $request): JsonResponse
     {
+        $responses = Http::pool(fn (Pool $pool) => [
+            $pool->as('users')->get('http://api-gateway-users.ddev.site/api/users'),
+            $pool->as('orders')->get('http://api-gateway-orders.ddev.site/api/orders'),
+        ]);
+
         return response()->json([
-            'users' => Http::get('http://api-gateway-users.ddev.site/api/users')
-                ->json(),
-            'orders' => Http::get('http://api-gateway-orders.ddev.site/api/orders')
-                ->json(),
+            'users' => $responses['users']->json(),
+            'orders' => $responses['orders']->json(),
         ]);
     }
 }
